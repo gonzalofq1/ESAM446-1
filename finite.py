51b9a45a6b8e682a2bdd0dfbabd8ddc4e8e88c82
Gonzalo Ferrandez Quinto <80678640+gonzalofq1@users.noreply.github.com>
2023-10-06 10:26:08 -0500

    Update finite.py

--- finite.py
+++ finite.py
@@ -36,7 +36,7 @@ class DifferenceUniformGrid(Difference):
         m = derivative_order
         n = convergence_order
 
-        r = int((m+1)/2)*2-1+n
+        r = n+m
         p = (r-1)/2
         S = np.zeros((r,r))
         ps = np.linspace(-p,p,r)
