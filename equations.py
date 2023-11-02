065c9632bc0c453005f314c3ad9836e2a730b21d
Gonzalo Ferrandez Quinto <80678640+gonzalofq1@users.noreply.github.com>
2023-11-02 00:40:34 -0500

    Update equations.py

--- equations.py
+++ equations.py
@@ -6,24 +6,6 @@ import timesteppers
 import scipy.sparse.linalg as spla
 
 
-class ReactionDiffusion2D:
-
-    def __init__(self, c, D, dx2, dy2):
-        pass
-
-    def step(self, dt):
-        pass
-
-
-class ViscousBurgers2D:
-
-    def __init__(self, u, v, nu, spatial_order, domain):
-        pass
-
-    def step(self, dt):
-        pass
-
-
 class ViscousBurgers:
     
     def __init__(self, u, nu, d, d2):
@@ -65,16 +47,60 @@ class Wave:
         self.F = lambda X: 0*X.data
 
 
+class ReactionDiffusion:
+
+    def __init__(self, c, d2, c_target, D):
+        self.X = StateVector([c])
+        N = len(c)
+        I = sparse.eye(N, N)
+        Z = sparse.csr_matrix((N, N))
+
+
+        self.M = I
+        L00 = -D*d2.matrix-I*c_target
+
+        if (type(c_target) == np.ndarray):
+            L00 = -D*d2.matrix-sparse.diags(c_target,0)
+
+        self.L = L00
+
+        f = lambda X: -X.data**2
+        self.F = f
+
+
+
 class SoundWave:
 
-    def __init__(self, u, p, d, rho0, gamma_p0):
-        pass
+    def __init__(self, u, p, d, rho0, gammap0):
+        self.X = StateVector([u, p])
+        N = len(u)
+        I = sparse.eye(N, N)
+        Z = sparse.csr_matrix((N, N))
 
+        M00 = I*rho0 
+        M01 = Z
+        M10 = Z
+        M11 = I
 
-class ReactionDiffusion:
-    
-    def __init__(self, c, d2, c_target, D):
-        pass
+        if (type(rho0) == np.ndarray):
+            M00 = sparse.diags(rho0,0)
+
+        self.M = sparse.bmat([[M00, M01],
+                              [M10, M11]])
+
+        L00 = Z
+        L01 =  d.matrix
+        L10 =  gammap0*d.matrix
+        L11 = Z
+
+        if (type(gammap0) == np.ndarray):
+            L10 = d.matrix @ sparse.diags(gammap0,0) 
+
+        self.L = sparse.bmat([[L00, L01],
+                              [L10, L11]])
+
+        f = lambda X: 0*X.data
+        self.F = f
 
 
 class ReactionDiffusion2D: 
